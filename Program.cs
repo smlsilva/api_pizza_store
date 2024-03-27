e095db7713f12f03830682704ed5c719ecd214bc
Samuel <silvasmlsss.02@gmail.com>
2024-03-26 21:32:31 -0300

    Organizando os endpoints para ter o código mais limpo e fácil de manter

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using pizzastore.pizza;
+using pizzastore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,64 +24,6 @@ if(app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
-var sabores = new List<Pizza> {
-    new Pizza { id = 1, nome = "Calabresa" },
-    new Pizza { id = 2, nome = "Brócolis"  },
-    new Pizza { id = 3, nome = "Frango"  },
-    new Pizza { id = 4, nome = "Catupiry"  },
-    new Pizza { id = 5, nome = "Peito de Peru"  }
-};
-
-app.MapGet("/cardapio", () => { 
-    return sabores;
-})
-.WithName("Todos os Sabores");
-
-app.MapGet("/cardapio/{id}", (int id) => { 
-    var sabor = sabores.Find(sabor => sabor.id == id);
-
-    if(sabor is null) {
-        return Results.NotFound("Nenhum Resultado foi encontrado!");
-    }
-
-    return Results.Ok(sabor);
-})
-.WithName("Pegando sabor especifico")
-.RequireAuthorization();
-
-app.MapPost("/cardapio", (Pizza pizza) => {
-    sabores.Add(pizza);
-    return Results.Created("",sabores);
-})
-.WithName("Inserindo um novo sabor de pizza")
-.RequireAuthorization();
-
-app.MapPut("/cardapio/{id}", (Pizza updateCardapio, int id) => {
-    var sabor = sabores.Find(sabor => sabor.id == id);
-
-    if(sabor is null) {
-        return Results.NotFound("Nenhum Resultado foi encontrado!");
-    }
-
-    sabor.nome = updateCardapio.nome;
-
-    return Results.Ok(sabor);
-})
-.WithName("Atualizando sabor específico")
-.RequireAuthorization();
-
-app.MapDelete("/cardapio/{id}", (int id) => { 
-    var sabor = sabores.Find(sabor => sabor.id == id);
-
-    if(sabor is null) {
-        return Results.NotFound("Nenhum Resultado foi encontrado!");
-    }
-
-    sabores.Remove(sabor);
-
-    return Results.Ok(sabor);
-})
-.WithName("Deletar um sabor do cardápio")
-.RequireAuthorization();
+app.RegisterPizzaEndpoints();
 
 app.Run();
